2178757e40a00d956c72b79b512b4339f086746b
Saara Korpela <smjkor@wppies.com>
2018-10-19 14:11:48 +0300

    New folder inger

---
+++ testfile.R
@@ -0,0 +1,26 @@
+# testfile only
+# you could in theory do a statistics plotter for certain excel format for your own use?
+# use bitbucket for secret projects with git
+
+
+# tee bitbucket gitillä uuteen kansikoon ja konffaa se
+
+# google sheets
+# for data you could use data transform, analysis and publishment as google spreadsheets API
+# some everyday problem that may be useful to visualize with R?
+# something that exports a report as pdf or google docs?
+
+
+# swirl?
+
+# use match.arg() to check arguments or defaults for objects and trycatching
+# use roxygen2 for documentation, devtools, kitr
+# tests folder for your testthat tests CMD sum during building of package
+# design robust/complete test for each function especially when using external imports
+# note: travis (unix), appveyor (win) will run when submitting to git and checks if package builds correctly
+
+# travis badge needs to be in README.md to be visible to users that travis is checking
+
+# no apple1, apple2 etc. use data structure apples to access apple[[2]]
+# check apropos() if function name already in use
+# a package for humastists? something that you cannot do in excel...
