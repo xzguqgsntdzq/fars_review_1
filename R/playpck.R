3fb30ef4f6ec2e236905a72584475e47deb29f42
Saara Korpela <smjkor@wppies.com>
2018-10-23 12:27:48 +0300

    DESCRIPITON license, name R test files Rbuildignore checked to contain yml files

---
+++ R/playpck.R
@@ -0,0 +1,62 @@
+
+
+# creating, writing, documenting, and testing an R package with releasing that package on GitHub
+
+#write a vignette to include in your package using knitr and R Markdown
+#write at least one test written using testthat
+#put your package on GitHub
+#set up the repository so that the package can be checked and built on Travis
+#Once your package has built on Travis and the build is passing with no errors, warnings, or notes you should add your Travis badge to the README.md file of your package repository.
+
+
+# export functions to usr with roxygen
+# license
+# readme
+# description metadata
+# function packages
+
+# or you could do a package that draws something based on specific -format data?
+
+# oltava joku mistä halutaan koottuja tilastoja ite tai
+# sitä on tarvittava OMAN datan analyysissä apuvälineenä
+# integroi jotain kaks alla mainittua raportiksi? query joku (ota huomioon et täyttää em. kriteerit)
+
+# google trends: googletetut R -paketit?
+# instagram likes
+# exoplanet data
+# natural sound specimens
+# eu government weather, crime etc. data
+# CIA world fatbook, military economics, population, infra etc.
+# UNICEF offers statistics on the situation of women and children worldwide.
+# World Health Organization offers world hunger, health, and disease statistics.
+# 1000 Genome Project
+# FB api public information
+# stock data
+
+# trending
+# cities vs. facebook public profiles?
+# patents?
+# trademarks?
+
+# ocean sediment waste
+
+# jotain lepakkoihin liittyvään joko integroituna tai
+# mahdollisuus yhdistää eri tietokantoja?
+#
+
+
+
+# teet jonkun itsellesi hyödyllisen julkaisualustan, jossa hyödynnät ensin toisten dataa
+# ja sitten alat täyttämään sitä omalla lepakkodatalla :P hahaha
+
+# what dta to merge?climate / agriculture data vs. rabies data vs. bat counts? nooo existing
+https://registry.opendata.aws/epa-rsei-pds/
+
+
+
+
+
+
+
+
+
