c4f9ff48b4c8db30d92c165028c53fe24a6d8ab6
Saara Korpela <smjkor@wppies.com>
2018-10-24 14:07:29 +0300

    yet another typo(s) corrected

--- tests/testthat/test_fars.R
+++ tests/testthat/test_fars.R
@@ -3,5 +3,5 @@
 # only one test_that made for this package, because data is not included in inst directory
 
 testthat::test_that("filename generated", {
-  testthat:expect_that(make_filename(as.integer(2010)), testthat::is_a(character))
+  testthat::expect_that(make_filename(as.integer(2010)), testthat::is_a("character"))
 })
